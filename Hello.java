aadcd6dc56cc51f2577eb903f8f1081a85277a2d
RDer <kirrotle@gmail.com>
2024-07-07 16:22:47 +0800

    [modify] Learn Inner class - member inner class

--- Hello.java
+++ Hello.java
@@ -1,18 +1,39 @@
-import java.net.Socket;
-
-public class Hello {//這個是Class Block
-    public static void main(String[] args) {//這個是Method Block
-        Test x = new Test();
+public class Hello {
+    public static void main(String[] args) {
+        //需記住不是Outer.Inner x = new Outer.Inner();
+        //需要先有外部的Outer,才可以創建內部的Inner
+        Outer.Inner x = new Outer().new Inner();
     }
 }
 
-class Test{
-    static {//static block
-        System.out.println("類載入記憶體時最先執行的程式碼");
+class Outer {
+
+    public Outer(){
+        Inner x = new Inner();
+        x.method();
     }
 
-    public Test(){
-        System.out.println("constructor 載入");
+    private void method(){
+        System.out.println("Outer method");
+    }
+
+    class Inner {
+
+        private void method(){
+            System.out.println("Inner method");
+        }
+
+         public Inner(){
+             System.out.println("我是內部類");
+             //當沒有Inner.method的方法可以直接呼叫到Outer.method
+             method();
+             //當內部也有一個method時就得這樣使用
+             //因為嚴格來說也是自己本類的成員
+             //感覺需要想的是this.num其實是Inner.this.num
+             //所以
+             Inner.this.method();
+             Outer.this.method();
+        }
     }
 }
 
