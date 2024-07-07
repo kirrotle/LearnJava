0934454302307c3427d6159f0f5c5029bae629de
RDer <kirrotle@gmail.com>
2024-07-07 13:58:41 +0800

    [modify] Learn constructor block

--- Hello.java
+++ Hello.java
@@ -1,12 +1,23 @@
 public class Hello {//這個是Class Block
     public static void main(String[] args) {//這個是Method Block
-        {//這個是Local Block
-            int x = 10;
-            System.out.println(x);
-        }
-        //因上面的x出大括號之後被釋放了
-        //所以下面可以再宣告一個x
-        int x = 20;
-        System.out.println(x);
+        Test x = new Test();
+        Test y = new Test(123);
     }
 }
+
+class Test{
+
+    {
+        System.out.println("所有constructor都會跑");
+    }
+
+    public Test(){
+        System.out.println("沒參數的constructor");
+    }
+
+    public Test(int x){
+        System.out.println("有參數的constructor");
+    }
+
+}
+
