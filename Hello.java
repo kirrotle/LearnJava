d18aa161d914cc5a7ef71b6ad22512b986239140
RDer <kirrotle@gmail.com>
2024-07-07 17:41:11 +0800

    [modify] learn inner function Class

--- Hello.java
+++ Hello.java
@@ -1,35 +1,22 @@
 public class Hello {
     public static void main(String[] args) {
-        //需注意到這邊不用new Outer().new Inner()了
-        //因為Inner是static
-        Outer.Inner x = new Outer.Inner();
+        Outer x = new Outer();
         x.method();
-
-        //如果內部方法也是static可以直接使用
-        Outer.Inner.method1();
     }
 }
 
-class Outer {
+class Outer{
 
     public void method(){
-        System.out.println("Outer method");
-    }
 
-    static class Inner {
-
-        public void method(){
-            System.out.println("Inner method");
-            //會無法使用,因為Inner是static無法讀取非static的class
-            //Outer.this.method();
-            //所以要先new之後才可以使用
-            Outer x = new Outer();
-            x.method();
+        class Inner{
+            public void method(){
+                System.out.println("Inner method");
+            }
         }
 
-        public static void method1(){
-            System.out.println("Inner static method");
-        }
+        Inner x = new Inner();
+        x.method();
     }
 
 }
