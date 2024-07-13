9f6612488dff0faeecc7cb80c555f297cfc74aef
RDer <kirrotle@gmail.com>
2024-07-13 15:09:20 +0800

    [modify] Learn Anonymous Inner Class

--- Hello.java
+++ Hello.java
@@ -1,23 +1,36 @@
 public class Hello {
     public static void main(String[] args) {
-        Outer x = new Outer();
-        x.method();
-    }
-}
+        //一般來說是使用class去繼承interface之後覆寫method方法
+        //test是interface的實現類
+        UseInter(new test());
 
-class Outer{
-
-    public void method(){
-
-        class Inner{
-            public void method(){
-                System.out.println("Inner method");
+        //Anonymous Inner Class是直接在程式碼中直接覆寫method方法
+        //節省還要再寫一個繼承inter的class
+        //下面的這一大串就是inter的實現類對象,只是是一個沒有名字的class而已
+        UseInter(new inter() {
+            @Override
+            public void method() {
+                System.out.println("123");
             }
-        }
-
-        Inner x = new Inner();
-        x.method();
+        });
     }
+
+    public  static  void UseInter(inter i){
+        i.method();
+    }
+
 
 }
 
+interface inter{
+    void method();
+}
+
+class test implements inter{
+
+    @Override
+    public void method() {
+        System.out.println("test implement inter");
+    }
+
+}
