82004fb76d5248f9efc7faefd4b3cb0b8da44ab0
RDer <kirrotle@gmail.com>
2024-07-14 15:47:27 +0800

    [modify] Learn Anonymous Inner Class And lLambda diff

--- Hello.java
+++ Hello.java
@@ -2,17 +2,15 @@ import java.util.Random;
 
 public class Hello {
     public static void main(String[] args) {
-        //這個是沒有省略的寫法
-        UseInter((String msg) -> {
-            return msg;
+
+        UseInter(new inter() {
+            @Override
+            public String ShowMsg(String msg) {
+                return "Anonymous Inner Class";
+            }
         });
 
-        //省略的寫法
-        //可以省略參數型別
-        //因只有一個參數,所以()省略
-        //因方法體只有一行所以可以省略{}跟結尾的;
-        //因這個function有回傳值,且因為上面的條件省略{},return也可以省略不寫
-        UseInter(msg -> msg + "省略的寫法");
+        UseInter(msg -> msg + "Lambda");
 
     }
 
@@ -22,8 +20,6 @@ public class Hello {
 
 }
 
-//如果Interface中只有一個function需要被實現的話,
-//就可以被稱為函數式編程接口
 @FunctionalInterface
 interface inter{
     String ShowMsg(String msg);
