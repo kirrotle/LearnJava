a47c7cc86bb0a40ebdfb2953913d6aa7f4c5f45f
RDer <kirrotle@gmail.com>
2025-02-09 20:15:09 +0800

    [modify] Learn JButton Class

--- Test/src/Jframe/JTest.java
+++ Test/src/Jframe/JTest.java
@@ -3,14 +3,23 @@ package Jframe;
 import javax.swing.*;
 public class JTest {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("456");
 
-        //設置窗口大小(px為單位)
+        JFrame frame = new JFrame("456");
         frame.setSize(500,400);
 
-        frame.setVisible(true);
+        //增加控鍵
+        JButton button = new JButton("Click Me");
+        button.setBounds(0,0,100,50);
+
+        //將控鍵新增至panel上
+        //frame.add(button);
+        frame.getContentPane().add(button);
+
+        //取消默認布局
+        frame.setLayout(null);
 
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
 
     }
 }
